b4f4db344ec2651e28cb33ecb97412eb24ae3345
Zachary Larson <Zach@Zacharys-MacBook-Pro.local>
2021-07-01 14:59:02 -0400

    day 4 commit practice

---
+++ Practice.js
@@ -0,0 +1,5 @@
+console.log('Start')
+setTimeout(function(){
+    console.log('Middle')
+},0)
+console.log('Done')
---
+++ peer.js
@@ -0,0 +1,26 @@
+// // todo #1: Print the odd numbers less than 100
+
+// for(let i=0; i<101; i++){
+// if (i % 2 === 1) {
+//     console.log(i)
+// }
+// }
+//     // if(i % 2 === 0) {
+//     //     console.log()
+//     // } else {
+//     //     console.log()
+//     // }
+
+//     Create a function that will convert from Celsius to Fahrenheit
+
+// for(let i=0; i<101; i++){
+//     console.log((i), 'degrees in Celsius equals', (i*1.8+32), 'in Farenheit')
+// }
+
+// function convertTemp (num1){
+//     console.log((num1), 'degrees in Celsius equals', (num1*1.8+32), 'in Farenheit')
+// }
+
+// convertTemp(10)
+
+//Calculate the sum of numbers in an array of numbers
